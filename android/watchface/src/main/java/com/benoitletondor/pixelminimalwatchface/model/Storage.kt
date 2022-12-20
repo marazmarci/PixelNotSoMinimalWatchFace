ec01d68829979b6c6927a3e60b224a3646bb3553
Benoit Letondor <benoit.letondor@gmail.com>
2022-12-20 17:01:50 +0100

    Fix complication secondary colors not being saved

--- android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/model/Storage.kt
+++ android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/model/Storage.kt
@@ -380,6 +380,54 @@ class StorageImpl(
                     DEFAULT_COMPLICATION_COLOR
                 } else { complicationColors.android12BottomRightColor.color }
             )
+            .putInt(
+                KEY_LEFT_SECONDARY_COMPLICATION_COLOR,
+                if( complicationColors.leftSecondaryColor.isDefault ) {
+                    DEFAULT_COMPLICATION_COLOR
+                } else { complicationColors.leftSecondaryColor.color }
+            )
+            .putInt(
+                KEY_MIDDLE_SECONDARY_COMPLICATION_COLOR,
+                if( complicationColors.middleSecondaryColor.isDefault ) {
+                    DEFAULT_COMPLICATION_COLOR
+                } else { complicationColors.middleSecondaryColor.color }
+            )
+            .putInt(
+                KEY_RIGHT_SECONDARY_COMPLICATION_COLOR,
+                if( complicationColors.rightSecondaryColor.isDefault ) {
+                    DEFAULT_COMPLICATION_COLOR
+                } else { complicationColors.rightSecondaryColor.color }
+            )
+            .putInt(
+                KEY_BOTTOM_SECONDARY_COMPLICATION_COLOR,
+                if( complicationColors.bottomSecondaryColor.isDefault ) {
+                    DEFAULT_COMPLICATION_COLOR
+                } else { complicationColors.bottomSecondaryColor.color }
+            )
+            .putInt(
+                KEY_ANDROID_12_TOP_LEFT_SECONDARY_COMPLICATION_COLOR,
+                if( complicationColors.android12TopLeftSecondaryColor.isDefault ) {
+                    DEFAULT_COMPLICATION_COLOR
+                } else { complicationColors.android12TopLeftSecondaryColor.color }
+            )
+            .putInt(
+                KEY_ANDROID_12_BOTTOM_LEFT_SECONDARY_COMPLICATION_COLOR,
+                if( complicationColors.android12BottomLeftSecondaryColor.isDefault ) {
+                    DEFAULT_COMPLICATION_COLOR
+                } else { complicationColors.android12BottomLeftSecondaryColor.color }
+            )
+            .putInt(
+                KEY_ANDROID_12_TOP_RIGHT_SECONDARY_COMPLICATION_COLOR,
+                if( complicationColors.android12TopRightSecondaryColor.isDefault ) {
+                    DEFAULT_COMPLICATION_COLOR
+                } else { complicationColors.android12TopRightSecondaryColor.color }
+            )
+            .putInt(
+                KEY_ANDROID_12_BOTTOM_RIGHT_SECONDARY_COMPLICATION_COLOR,
+                if( complicationColors.android12BottomRightSecondaryColor.isDefault ) {
+                    DEFAULT_COMPLICATION_COLOR
+                } else { complicationColors.android12BottomRightSecondaryColor.color }
+            )
             .apply()
     }
 
