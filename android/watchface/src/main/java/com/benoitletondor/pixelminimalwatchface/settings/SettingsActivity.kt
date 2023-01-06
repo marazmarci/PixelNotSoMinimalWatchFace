ce54e5f2204476416ff7eb9180f77693defd6ff4
Benoit Letondor <benoit.letondor@gmail.com>
2023-01-06 23:53:35 +0100

    Code cleaning and version bump

--- android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/settings/SettingsActivity.kt
+++ android/watchface/src/main/java/com/benoitletondor/pixelminimalwatchface/settings/SettingsActivity.kt
@@ -18,9 +18,6 @@ package com.benoitletondor.pixelminimalwatchface.settings
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.ColorInt
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
 import androidx.wear.compose.navigation.SwipeDismissableNavHost
@@ -32,19 +29,8 @@ import com.benoitletondor.pixelminimalwatchface.model.ComplicationColor
 import com.benoitletondor.pixelminimalwatchface.model.ComplicationColorsProvider
 import com.benoitletondor.pixelminimalwatchface.model.ComplicationLocation
 import com.benoitletondor.pixelminimalwatchface.settings.screens.ComplicationColorScreen
-import com.benoitletondor.pixelminimalwatchface.settings.screens.ComplicationColorScreenResultKey
 import com.benoitletondor.pixelminimalwatchface.settings.screens.SettingsScreen
 import com.benoitletondor.pixelminimalwatchface.settings.screens.WidgetConfigurationScreen
-import kotlinx.coroutines.*
-import kotlin.coroutines.resume
-
-private const val ROUTE_START = "settings"
-
-private const val ROUTE_COLOR_DEFAULT_ARG = "default"
-private const val ROUTE_COLOR = "color/{$ROUTE_COLOR_DEFAULT_ARG}"
-
-private const val ROUTE_WIDGET_LOCATION_ARG = "location"
-private const val ROUTE_WIDGET = "widget/{$ROUTE_WIDGET_LOCATION_ARG}"
 
 class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -97,31 +83,4 @@ class SettingsActivity : ComponentActivity() {
     }
 }
 
-fun NavHostController.navigateToWidgetSelectionScreen(complicationLocation: ComplicationLocation) {
-    navigate(ROUTE_WIDGET.replace("{${ROUTE_WIDGET_LOCATION_ARG}}", complicationLocation.name))
-}
 
-suspend fun NavHostController.navigateToColorSelectionScreen(@ColorInt defaultColor: Int): ComplicationColor? = suspendCancellableCoroutine { continuation ->
-    var listener:  NavController.OnDestinationChangedListener? = null
-    val destination = currentDestination
-    listener = NavController.OnDestinationChangedListener { _, newDestination, _ ->
-        if (newDestination != destination) {
-            return@OnDestinationChangedListener
-        }
-
-        listener?.let { removeOnDestinationChangedListener(it) }
-
-        val result = currentBackStackEntry?.savedStateHandle?.get<ComplicationColor>(ComplicationColorScreenResultKey)
-        if (continuation.isActive) {
-            continuation.resume(result)
-        }
-    }
-
-    navigate(ROUTE_COLOR.replace("{${ROUTE_COLOR_DEFAULT_ARG}}", defaultColor.toString()))
-
-    addOnDestinationChangedListener(listener)
-
-    continuation.invokeOnCancellation {
-        removeOnDestinationChangedListener(listener)
-    }
-}
